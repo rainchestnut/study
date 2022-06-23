944a87fdcc17a00271d2050f6a914d71bfd6a3d5
王冠 <wangguan@aglines.com>
2022-06-23 19:26:08 +0800

    fix

--- demo/src/demo.jl
+++ demo/src/demo.jl
@@ -13,31 +13,32 @@ julia>
 """
 module demo
 const ROOT_DIR = normpath(joinpath(@__DIR__, ".."))
-    function pathPush()
-        push!(LOAD_PATH,joinpath(ROOT_DIR,"src","julia","func"))
-        push!(LOAD_PATH,joinpath(ROOT_DIR,"src","julia","entity"))
-        push!(LOAD_PATH,joinpath(ROOT_DIR,"src","julia","util"))
-        push!(LOAD_PATH,joinpath(ROOT_DIR,"src","julia","main"))
-        push!(LOAD_PATH,joinpath(ROOT_DIR,"resources"))
-    end
-    pathPush()
-    using DemoTest1;
-    using MySQL
-    using Tables
-    using DBUtil
-    using DBStudy
-    # println(DemoTest1.getStringCut("受命于天既寿永昌",1,6));
-    # DemoTest1.readfileline(pwd() * "\\demo\\resources\\" * "Pride and Prejudice.txt");
-    # DemoTest1.readfileline("demo\\resources\\" * "Pride and Prejudice.txt");
-    # DemoTest1.getWordFrequency("demo\\resources\\" * "Pride and Prejudice.txt");
-    # @time println(DemoTest1.getCommonWord("demo\\resources\\" * "Pride and Prejudice.txt",10))
-    # @time println(DemoTest1.getCommonWord("demo\\resources\\" * "Pride and Prejudice.txt"))
-    # wordCount = DemoTest1.getWordFrequency(joinpath(ROOT_DIR,"resources","Pride and Prejudice.txt"))
-    # wordSta = DemoTest1.sumarrayofstatistics(wordCount)
-    # wordKeys = [keys(wordCount)...]
-    # randomWord = DemoTest1.getRandomWord(wordKeys , wordSta)
-    # println(randomWord)
-    sql = "select * from t_sys_user limit 10"
-    result = DBUtil.querylist("julia_study",sql;dt=DBStudy.SysUser)
-    println(result)
+function pathPush()
+    push!(LOAD_PATH, joinpath(ROOT_DIR, "src", "julia", "func"))
+    push!(LOAD_PATH, joinpath(ROOT_DIR, "src", "julia", "entity"))
+    push!(LOAD_PATH, joinpath(ROOT_DIR, "src", "julia", "util"))
+    push!(LOAD_PATH, joinpath(ROOT_DIR, "src", "julia", "main"))
+    push!(LOAD_PATH, joinpath(ROOT_DIR, "resources"))
+end
+pathPush()
+using DemoTest1
+using DBUtil
+using DBStudy
+# println(DemoTest1.getStringCut("受命于天既寿永昌",1,6));
+# DemoTest1.readfileline(pwd() * "\\demo\\resources\\" * "Pride and Prejudice.txt");
+# DemoTest1.readfileline("demo\\resources\\" * "Pride and Prejudice.txt");
+# DemoTest1.getWordFrequency("demo\\resources\\" * "Pride and Prejudice.txt");
+# @time println(DemoTest1.getCommonWord("demo\\resources\\" * "Pride and Prejudice.txt",10))
+# @time println(DemoTest1.getCommonWord("demo\\resources\\" * "Pride and Prejudice.txt"))
+# wordCount = DemoTest1.getWordFrequency(joinpath(ROOT_DIR,"resources","Pride and Prejudice.txt"))
+# wordSta = DemoTest1.sumarrayofstatistics(wordCount)
+# wordKeys = [keys(wordCount)...]
+# randomWord = DemoTest1.getRandomWord(wordKeys , wordSta)
+# println(randomWord)
+sql = "update t_sys_user set remark = #{remark} where id = #{id}"
+@time result = DBUtil.update(sql, Dict{AbstractString,Any}("remark" => "系统用户", "id" => "1"))
+println(result)
+sql1 = "select * from t_sys_user where id = 1 limit 10"
+@time result1 = DBUtil.queryone(sql1, Dict{AbstractString,Any}(), dt=DBStudy.SysUser)
+println(result1)
 end
--- demo/src/julia/util/DBUtil.jl
+++ demo/src/julia/util/DBUtil.jl
@@ -1,73 +1,127 @@
 module DBUtil
-using JSON
-using MySQL
+using Pkg
+using TOML
+using DBInterface
 using DataStructures
 using Tables
+
+global dbmap = missing
 
 function getconnect end
 function init! end
 function querylist end
-
-dbmap = missing
+function queryone end
 struct DBLink
-    host::AbstractString
-    port::Int
-    username::AbstractString
-    password::AbstractString
-    schema::AbstractString
+    modu::AbstractString
+    conn::AbstractString
 end
-function getconnect(key::AbstractString=[keys(dbmap)...][1])
-    global dbmap
-    link = dbmap[key]
-    return DBInterface.connect(MySQL.Connection, link.host, link.username, link.password, db=link.schema)
+function update(sql::AbstractString, param::Dict{}, conn::DBInterface.Connection)
+    stmt = buildstmt(sql, param, conn)
+    res = DBInterface.execute(stmt...)
+    effect = res.rows_affected
+    DBInterface.close!(stmt[1])
+    return effect
 end
-
-
-function querylist(conn::DBInterface.Connection, sql::AbstractString; dt::DataType)
-    dbiterator = DBInterface.execute(conn, sql)
-    list::Vector{dt} = []
-    clmnamemap = missing
-    for item in dbiterator
-        if clmnamemap === missing
-            clmnames = string.(Tables.columnnames(item))
-            clmnamemap = Dict(replace(clm ,"_"=>"") => clm for clm in clmnames)
-            fieldnames = [keys(clmnamemap)...]
-        end
-        args = []
-        for fldname in string.(Base.fieldnames(dt))
-            if fldname in(fieldnames)
-                push!(args,Tables.getcolumn(item,Symbol(clmnamemap[fldname])))
-            else
-                push!(args,nothing)  
-            end
-        end
-        push!(list,dt(args...))
-    end
-    return list
-end
-function querylist(connstr::AbstractString, sql::AbstractString; dt::DataType)
+function update(sql::AbstractString, param::Dict{}, connstr::AbstractString=[keys(dbmap)...][1])
     dbconnect = getconnect(connstr)
     try
-        return querylist(dbconnect,sql,dt=dt)
+        return update(sql, param, dbconnect)
     finally
         close!(dbconnect)
     end
 end
-function init!(; fresh::Bool=false)
-    global dbmap
-    if dbmap === missing || fresh
-        path = normpath(joinpath(@__DIR__, "..", "..", ".."), "resources", "db.json")
-        open(path, "r") do io
-            dbjsonmap = JSON.parse(io)
-            dbmap = SortedDict(k => DBLink(string(v["host"]), Int(v["port"]), string(v["username"]), string(v["password"]), string(v["schema"])) for (k, v) in dbjsonmap)
+function querylist(sql::AbstractString, param::Dict{}, conn::DBInterface.Connection; dt::DataType=Dict{AbstractString,Any})
+    stmt = buildstmt(sql, param, conn)
+    list::Vector{dt} = []
+    DBInterface.execute(stmt...) do dbiterator
+        clmnamemap = missing
+        for item in dbiterator
+            if clmnamemap === missing
+                clmnames = string.(Tables.columnnames(item))
+                clmnamemap = Dict(replace(clm, "_" => "") => clm for clm in clmnames)
+            end
+            push!(list, convertobject(item, clmnamemap; dt=dt))
         end
-        println("数据库配置初始化完成")
+    end
+    DBInterface.close!(stmt[1])
+    return list
+end
+function querylist(sql::AbstractString, param::Dict{}, connstr::AbstractString=[keys(dbmap)...][1]; dt::DataType=Dict{AbstractString,Any})
+    dbconnect = getconnect(connstr)
+    try
+        return querylist(sql, param, dbconnect; dt=dt)
+    finally
+        close!(dbconnect)
+    end
+end
+function queryone(sql::AbstractString, param::Dict{}, conn::DBInterface.Connection; dt::DataType=Dict{AbstractString,Any})
+    stmt = buildstmt(sql, param, conn)
+    dbiterator = DBInterface.execute(stmt...)
+    clmnamemap = missing
+    for item in dbiterator
+        if clmnamemap === missing
+            clmnames = string.(Tables.columnnames(item))
+            clmnamemap = Dict(replace(clm, "_" => "") => clm for clm in clmnames)
+        end
+        return convertobject(item, clmnamemap; dt=dt)
+    end
+    DBInterface.close!(stmt[1])
+    return list
+end
+function queryone(sql::AbstractString, param::Dict{}, connstr::AbstractString=[keys(dbmap)...][1]; dt::DataType=Dict{AbstractString,Any})
+    dbconnect = getconnect(connstr)
+    try
+        return queryone(sql, param, dbconnect; dt=dt)
+    finally
+        close!(dbconnect)
+    end
+end
+function getconnect(key::AbstractString=[keys(dbmap)...][1])
+    conn = dbmap[key].conn
+    eva = Meta.parse(conn)
+    println(eva)
+    eval(eva)
+end
+function buildstmt(sql::AbstractString, param::Dict{AbstractString,Any}, conn::DBInterface.Connection)
+    matcher = eachmatch(r"#{(.*?)}", sql)
+    if matcher === nothing
+        params = []
+    else
+        params = [param[tag.captures...] for tag in matcher]
+        sql = replace(sql, r"#{(.*?)}" => "?")
+
+    end
+    println(sql, params)
+    stmt = DBInterface.prepare(conn, sql)
+    return stmt, params
+end
+function convertobject(item::Tables.AbstractRow, clmnamemap::Dict; dt::DataType=Dict{AbstractString,Any})
+    fieldnames = [keys(clmnamemap)...]
+    if dt <: Dict
+        return Dict(k => Tables.getcolumn(item, Symbol(v)) for (k, v) in clmnamemap)
+    else
+        args = []
+        for fldname in string.(Base.fieldnames(dt))
+            if fldname in (fieldnames)
+                push!(args, Tables.getcolumn(item, Symbol(clmnamemap[fldname])))
+            else
+                push!(args, nothing)
+            end
+        end
+        return dt(args...)
     end
 end
 function close!(conn::DBInterface.Connection)
     DBInterface.close!(conn)
 end
-
-
-init!()
+if dbmap === missing
+    path = normpath(joinpath(@__DIR__, "..", "..", ".."), "resources", "db.toml")
+    dbjsonmap = TOML.parsefile(path)
+    global dbmap = SortedDict(k => DBLink(string(v["modu"]), string(v["conn"])) for (k, v) in dbjsonmap)
+    for item in values(dbmap)
+        db = Symbol(item.modu)
+        eval(:(using $db))
+    end
+    println("数据库配置初始化完成了")
+end
 end
